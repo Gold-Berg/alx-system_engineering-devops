2acb2dd3d1fa9d91f2c10053b6b80a47254d7d38
Gold-Berg <igbinobaeseosa0@gmail.com>
2024-02-27 20:09:40 -0800

    Task 100

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-# Transfers a file from a client to a server
-if [ $# -lt 3 ]; then
-    echo 'Usage: 0-transfer_file PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY'
-    exit 1
-fi
-scp -i "$4" -o StrictHostKeyChecking=no "$1" "$3@$2:~/$1"
+# Installs a Nginx server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
