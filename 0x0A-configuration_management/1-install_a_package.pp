a5e429533f0f6eb0c44d5fb027646e46442a4260
Gold-Berg <igbinobaeseosa0@gmnail.com>
2024-02-28 20:46:16 -0800

    Tasks 1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Installs puppet-lint, version 2.1.1
+# File: install_flask.pp
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
+
